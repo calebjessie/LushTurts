f85efb4eaff0d8922be5b9eaa67239a91497951d
Caleb Jessie <cjjessie793@gmail.com>
2020-12-30 00:53:57 -0500

    Term test

--- hub/apis/monitor.lua
+++ hub/apis/monitor.lua
@@ -1,5 +1,3 @@
--- Display stats upon running startup.lua
-
 local func = { }
 local surface = require("/lushTurts/apis/surface")
 local monitor = peripheral.find("monitor")
@@ -7,18 +5,28 @@ local monitor = peripheral.find("monitor")
 term.redirect(monitor)
 monitor.setTextScale(.5)
 
-local w, h = monitor.getSize()
+local width, height = monitor.getSize()
 
 function func.initDisplay()
     monitor.clear()
     createHeader()
+    createTerm()
 end
 
 function createHeader()
-    local headerImg = surface.load("/lushTurts/images/headerImg")
-    local hSurf = surface.create(headerHeight, headerWidth)
-    hSurf:drawSurface(headerImg, 0, 0)
+    local headerImg = surface.load("/lushTurts/images/header")
+    local hSurf = surface.create(width, 8)
+    hSurf:drawSurfaceSmall(headerImg, 0, 0)
     hSurf:output()
+end
+
+function createTerm()
+    local tSurf = surface.create((width/2), (height-8))
+    tSurf:fillRect(0, 0, (width/2), (height-8), colors.red)
+end
+
+function createStatus()
+
 end
 
 function updateDisplay()
